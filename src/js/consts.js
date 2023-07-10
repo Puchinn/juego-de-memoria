a001994fd2fd22a3cb3e650c8a96cb082d7f37e5
Esteban Sayago <sayagoo823@gmail.com>
2023-07-10 18:50:48 -0300

    agregar los tamaños del tablero

--- src/js/consts.js
+++ src/js/consts.js
@@ -1,3 +1,11 @@
+export const tamaños = {
+  '4x3': { clase: '4x3', cartasUnicas: 6 },
+  '5x4': { clase: '5x4', cartasUnicas: 10 },
+  '6x6': { clase: '6x6', cartasUnicas: 18 },
+  '7x6': { clase: '7x6', cartasUnicas: 21 },
+  '9x6': { clase: '9x6', cartasUnicas: 27 },
+}
+
 export const contenidoTarjetas = [
   {
     valor: '🎵',
@@ -34,5 +42,50 @@ export const contenidoTarjetas = [
   },
   {
     valor: '🔥',
+  },
+  {
+    valor: '🌑',
+  },
+  {
+    valor: '🥚',
+  },
+  {
+    valor: '🚀',
+  },
+  {
+    valor: '👄',
+  },
+  {
+    valor: '👀',
+  },
+  {
+    valor: '🏳‍🌈',
+  },
+  {
+    valor: '🥤',
+  },
+  {
+    valor: '🎮',
+  },
+  {
+    valor: '💻',
+  },
+  {
+    valor: '🎱',
+  },
+  {
+    valor: '🐵',
+  },
+  {
+    valor: '👻',
+  },
+  {
+    valor: '⭐',
+  },
+  {
+    valor: '🍣',
+  },
+  {
+    valor: '🍨',
   },
 ]
